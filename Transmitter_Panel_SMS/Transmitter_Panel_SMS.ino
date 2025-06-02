17c248aea29635f4b660ac57b2b99ad50ccfc7e9
Pasan1021 <167863121+Pasan1021@users.noreply.github.com>
2025-06-02 10:27:51 +0530

    Transmitter Panel SMS Sending
    Send SMS to 5 predefined numbers

---
+++ Transmitter_Panel_SMS/Transmitter_Panel_SMS.ino
@@ -0,0 +1,209 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <WiFi.h>  // Include the WiFi library for MAC address
+#include <ArduinoJson.h>
+
+String gsm_send_serial(String command, int delay);
+
+#define SerialMon Serial
+#define SerialAT Serial1
+#define GSM_PIN ""
+
+// Your GPRS credentials
+const char apn[] = "dialogbb";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+// MQTT details
+String broker = "mqtt.datacake.co";
+String MQTTport = "8883";
+
+#define UART_BAUD 115200
+
+#define MODEM_TX 32
+#define MODEM_RX 33
+#define GSM_RESET 21
+
+#define D1 34 // Fire_Alarm_Input
+#define D2 35 // Fault_Alarm_Input
+#define D3 14 // Mains_Fails_Alarm_Input
+#define D4 13
+#define D5 5
+#define R0 12
+#define R1 2
+#define R2 27
+#define R3 4
+#define R4 23
+#define R5 18
+
+#define DEBOUNCE_DELAY 50 
+
+bool FEA_Status = false;
+bool FTA_Status = false;
+bool MSA_Status = false ;
+
+bool inputStatus[3] = {false, false, false}; // FEA, FTA, MSA
+
+//Debouncing and states
+unsigned long lastDebounceTime[3] = {0, 0, 0};
+bool lastStableState[3] = {HIGH, LOW, LOW};
+bool currentState[3] = {HIGH, LOW, LOW};
+bool processing = false;
+
+// Predefined phone numbers
+const char* phoneNumbers[5] = {
+   "+94761111111",
+   "+94771111111"
+   "+94701111111",
+   "+94741111111",
+   "+94781111111",
+  
+};
+
+// Alarm messages
+const char* smsMessages[3][2] = {
+  { "FACP - Fire Alarm Activated at Cooling Plant", "FACP - Fire Alarm CLEARED at Cooling Plant" },
+  { "FACP - Fault reported at Cooling Plant", "FACP - Fault cleared at Cooling Plant" },
+  { "FACP – Mains Power Fail reported at Cooling Plant", "FACP - Mains Power Fail cleared at Cooling Plant" }
+};
+
+void setup() {
+  // Set console baud rate
+  Serial.begin(115200);
+  delay(10);
+  SerialAT.begin(UART_BAUD, SERIAL_8N1, MODEM_RX, MODEM_TX);
+
+  delay(2000);
+  pinMode(GSM_RESET, OUTPUT);
+  digitalWrite(GSM_RESET, HIGH);  // RS-485
+  delay(2000);
+  
+  pinMode(D1, INPUT_PULLUP); 
+  pinMode(D2, INPUT_PULLUP); 
+  pinMode(D3, INPUT_PULLUP); 
+  pinMode(D4, INPUT);
+  pinMode(D5, INPUT);
+  pinMode(R0, OUTPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
+  pinMode(R3, OUTPUT);
+  pinMode(R4, OUTPUT);
+  pinMode(R5, OUTPUT);
+
+  Init();
+  connectToGPRS();
+}
+
+void loop() {
+  if (processing) return;
+  processing = true;
+
+  readInputsAndCheckAlarms();
+
+  processing = false;
+  delay(50); 
+
+}
+
+void readInputsAndCheckAlarms() {
+  unsigned long now = millis();
+  currentState[0] = digitalRead(D1);
+  currentState[1] = digitalRead(D2);
+  currentState[2] = digitalRead(D3);
+
+  for (int i = 0; i < 3; i++) {
+    if (currentState[i] != lastStableState[i]) {
+      if ((now - lastDebounceTime[i]) > DEBOUNCE_DELAY) {
+        lastDebounceTime[i] = now;
+        lastStableState[i] = currentState[i];
+
+        inputStatus[i] = interpretAlarmStatus(i, currentState[i]);
+        handleAlarmStateChange(i, inputStatus[i]);
+        break;
+      }
+    }
+  }
+}
+
+bool interpretAlarmStatus(int idx, bool state) {
+  if (idx == 0) return state == LOW;  // Fire
+  else return state == HIGH;          // Fault / Mains
+}
+
+void handleAlarmStateChange(int idx, bool status) {
+  String topic;
+  String payload = String(status);
+
+  String smsMessage = smsMessages[idx][status ? 0 : 1]; // Get proper message
+  Serial.println("[STATE CHANGE] Alarm " + String(idx) + ": " + (status ? "ACTIVATED" : "CLEARED") + " -> " + smsMessage);
+
+  // Send SMS
+  sendSMS(smsMessage);
+
+}
+
+void sendSMS(String message) {
+  for (int i = 0; i < 5; i++) {
+    gsm_send_serial("AT+CMGF=1", 1000); // Set SMS text mode
+    gsm_send_serial("AT+CSCS=\"GSM\"", 500); // Use GSM character set
+
+    String cmd = String("AT+CMGS=\"") + phoneNumbers[i] + "\"";
+    gsm_send_serial(cmd, 1000);
+    gsm_send_serial(message + "\x1A", 5000); // Send message with Ctrl+Z
+  }
+}
+
+
+void Init(void) {                        // Connecting with the network and GPRS
+  delay(5000);
+  gsm_send_serial("AT+CFUN=1", 10000);
+  gsm_send_serial("AT+CPIN?", 10000);
+  gsm_send_serial("AT+CSQ", 1000);
+  gsm_send_serial("AT+CREG?", 1000);
+  gsm_send_serial("AT+COPS?", 1000);
+  gsm_send_serial("AT+CGATT?", 1000);
+  gsm_send_serial("AT+CPSI?", 500);
+  String cmd = "AT+CGDCONT=1,\"IP\",\"" + String(apn) + "\"";
+  gsm_send_serial(cmd, 1000);
+  //gsm_send_serial("AT+CGDCONT=1,\"IP\",\"dialogbb\"", 1000);
+  gsm_send_serial("AT+CGACT=1,1", 1000);
+  gsm_send_serial("AT+CGATT?", 1000);
+  gsm_send_serial("AT+CGPADDR=1", 500);
+}
+
+void connectToGPRS(void) {
+  gsm_send_serial("AT+CGATT=1", 1000);
+  String cmd = "AT+CGDCONT=1,\"IP\",\"" + String(apn) + "\"";
+  gsm_send_serial(cmd, 1000);
+  //gsm_send_serial("AT+CGDCONT=1,\"IP\",\"dialogbb\"", 1000);
+  gsm_send_serial("AT+CGACT=1,1", 1000);
+  gsm_send_serial("AT+CGPADDR=1", 500);
+}
+
+bool isNetworkConnected() {
+  String response = gsm_send_serial("AT+CREG?", 3000);
+  return (response.indexOf("+CREG: 0,1") != -1 || response.indexOf("+CREG: 0,5") != -1);
+}
+
+bool isGPRSConnected() {
+  String response = gsm_send_serial("AT+CGATT?", 3000);
+  return (response.indexOf("+CGATT: 1") != -1);
+}
+
+String gsm_send_serial(String command, int timeout) {
+  String buff_resp = "";
+  Serial.println("Send ->: " + command);
+  SerialAT.println(command);
+  unsigned long startMillis = millis();
+  
+  while (millis() - startMillis < timeout) {
+    while (SerialAT.available()) {
+      char c = SerialAT.read();
+      buff_resp += c;
+    }
+    delay(10); // Small delay to allow for incoming data to accumulate
+  }
+  
+  Serial.println("Response ->: " + buff_resp);
+  return buff_resp;
+}
